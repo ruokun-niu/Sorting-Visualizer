608830aea8d41aec3edbc94bf928c3467c2b1b56
Ruokun (Tommy) Niu <ruokunniu@gmail.com>
2021-11-20 20:33:39 -0500

    Created the sorting vis class and constructor

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class SortingVisualizer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            arry:[]
+        };
+    }
+
+}
