0b845f6536ddabf925ba14af3b7ba99bb5952d65
Ruokun (Tommy) Niu <ruokunniu@gmail.com>
2021-11-22 17:15:26 -0500

    new array button and merge sort
    Created button for randomizing array; write basic merge sort algorithm

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -17,22 +17,30 @@ export default class SortingVisualizer extends React.Component {
 
     resetArray() {
         const array = [];
-        for (let i = 0; i < 100/*NUMBER_OF_ARRAY_BARS*/; i++) {
+        for (let i = 0; i < 310/*NUMBER_OF_ARRAY_BARS*/; i++) {
           array.push(randomIntFromInterval(5, 730));
         }
         this.setState({array});
     }
 
+    mergeSort() {
+
+    }
+
+    
     render() {
         const {array} = this.state;
-
+        //style = {{height: `${value}px`}}> 
+        //height = number of value pixels
         return (
             <div className='array-container'>
             {array.map((value, idx) => (
-                <div className="array-bar" key={idx}>
-                      {value}
+                <div className="array-bar" key={idx} 
+                style = {{height: `${value}px`}}> 
                   </div>
               ))}
+            <button onClick={() =>this.resetArray()}> Generate New Array  </button>
+            <button onClick={() => this.mergeSort()}>Merge Sort</button>
             </div>
         );
     }
